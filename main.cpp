72552ad8d3ccf313715ec2cf384bf771351ccdd4
Rishi <rishiraj0211@gmail.com>
2016-09-29 12:20:53 -0400

    testing

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout <<"Hello World!" << endl; // Print hello world to screen followed by end line (end1)
+  return 0; // exit program
+}
